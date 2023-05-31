65c19f2fa7bb58edd6dc859cef2cd7421d9f6a2a
tosinga <tosinga@student.codam.com>
2023-05-31 16:46:21 +0200

    Hallo

--- ex02/src/VectorMerge.cpp
+++ ex02/src/VectorMerge.cpp
@@ -19,7 +19,10 @@ void    PmergeMe::mergeVec(std::vector<int>& vec, int beginIndex, int middleInde
     int rightIndex = 0;
     int leftIndex = 0;
     
-    // merge the right and left subvectors back in the original vec, in order  
+    // merge the right and left subvectors back in the original vec, in order
+    // 4 options: 
+    //  1 & 2:  Either of the vectors is already merged
+    //  2 & 3   Either   
     for (int i = beginIndex; i <= endIndex; i++)
     {
         // if the right index reaches the end of that index, all elements of the right vec has been merged.
@@ -55,42 +58,46 @@ void    PmergeMe::vectorInsertionSort(std::vector<int>& vec, int beginIndex, int
 {
     for (int i = beginIndex; i < endIndex; i++)
     {
-        // store the first unsorted element & keep index
+        // 1. Store the first unsorted element & keep this index (the first time this is index 1)
+        // 2. Check  if we are not at the begining && if the value of the first unsorted element is smaller than the previous value.
+        // 3. If no: skip and keep in place.
+        // 4. If yes: Swap the elements.
         int temp = vec[i + 1];
         int index = i + 1;
 
-        //loop to shift elements, as long as the element is greater than the temp
         while (index > beginIndex && vec[index - 1] > temp)
         {
-            // shift temp val to the right for as long as necessary
             vec[index] = vec[index - 1];
             index--;
         }
-        // when the right spot is found, put the temp value in the right position
         vec[index] = temp;
     }
 }
 
 void    PmergeMe::vectorMergeSort(std::vector<int>& vec, int beginIndex, int endIndex)
 {
-    // First insertion sort, then merge the rest. 
-    // set changesort to the amount where to use insertion sort. if changeSort = 1, the algorithm will work as merge sort. if changeSort = N, only insertion sort will be used
+    // 1. Divide in subvectors until 'Change'
+    // 2. Use insertion sort for the subvectors of 'change' size.
+    // 3. Use merge sort for the remainder of the subvectors
+
+    // changesort: if changeSort = 1, the algorithm will work as merge sort. if changeSort = N, only insertion sort will be used
+
     int     changeSort = 5;
     int     middleIndex;
 
-    // Skip merge sort while the halves are smaller than changeSort (It skips to start with insertion sort)
-    // If the halves are bigger than changeSort: Merge sort will kick in!
     if (endIndex - beginIndex > changeSort)
     {
-        // Take the middle index to divide the vector in halves
+        // Take the middle index to divide  the vector in halves
         middleIndex = (beginIndex + endIndex) / 2;
-        // Recursively call this function for both halves
+
+        // Recursively call this function for both halves until halves of size 'change' is left
         vectorMergeSort(vec, beginIndex, middleIndex);
         vectorMergeSort(vec, middleIndex + 1, endIndex);
+
+        // call merge function
         mergeVec(vec, beginIndex, middleIndex, endIndex);
     }
-    // Call insertion sort for each sub array
+    // Call insertion sort for each sub array of changeSort size
     else
         vectorInsertionSort(vec, beginIndex, endIndex);
-
 }
